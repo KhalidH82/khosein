1c325ab47bf37779ac8074b4455d764b0c3056b3
Khalid Hosein <dieselk@optonline.com>
2025-03-09 14:48:41 -0400

    Update Hero.vue

--- components/Hero.vue
+++ components/Hero.vue
@@ -18,10 +18,10 @@
             </p>
             <div class="w-full flex flex-col sm:flex-row gap-4 animate-fade-up animation-delay-400">
               <NuxtLink to="/#projects" class="w-full sm:w-auto">
-                <InteractiveHoverButton text="View Projects" class="w-full sm:w-48" />
+                <InteractiveHoverButton text="View Projects" class="w-full sm:w-auto min-w-[180px]" />
               </NuxtLink>
               <NuxtLink to="/#contact" @click.prevent="scrollToContact" class="w-full sm:w-auto">
-                <InteractiveHoverButton text="Contact" class="w-full sm:w-48" />
+                <InteractiveHoverButton text="Contact" class="w-full sm:w-auto min-w-[180px]" />
               </NuxtLink>
             </div>
           </div>
